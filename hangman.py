65bf599e49cd2c4359c3692e749e30fb5a88a748
lavamitt <lavamitt@berkeley.edu>
2019-07-21 19:56:38 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -17,24 +17,28 @@ guesses = []
 maxfails = 7
 fails = 0
 
+# Main while loop
 while fails < maxfails:
+	
+	#Prints the current solved word so far & guesses
 	print(" ".join(current_word))
 	print("You have guessed " + str(guesses) + " so far")
 
 	guess = input("Guess a letter: ")
 
-	# check if the guess is valid: Is it one letter? Have they already guessed it?
+	# Check if the guess is valid: Is it one letter?
 	while len(guess) != 1:
 		print("Invalid input")
 		guess = input("Guess a letter: ")
 
+	# Have they already guessed it?
 	while guess in guesses:
 		print("You have already guessed this letter")
 		guess = input("Guess a letter: ")
 
 	guesses.append(guess)
 
-	# check if the guess is correct: Is it in the word? If so, reveal the letters!
+	# Check if the guess is correct: Is it in the word? If so, reveal the letters!
 
 	if guess in word:
 		for i in range(len(word)):
@@ -47,4 +51,5 @@ while fails < maxfails:
 		fails = fails + 1
 		print("You have " + str(maxfails - fails) + " tries left!")
 
+# If exited out of the while loop, then we must have ran out of lives!
 print("You ran out of lives! You lose")
